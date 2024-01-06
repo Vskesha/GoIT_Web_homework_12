f4801ce77354071cdcb2971ae59547757d879ac3
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-01-06 18:08:14 +0200

    updated functions and routes for contacts

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -1,32 +1,32 @@
 from datetime import datetime
 from typing import Union
 
-from sqlalchemy import func
+from sqlalchemy import func, and_
 from sqlalchemy.orm import Session
 
-from src.database.models import Contact
+from src.database.models import Contact, User
 from src.schemas import ContactModel
 
 
-async def get_contacts(db: Session, skip: int, limit: int, favorite: Union[bool, None] = None):
-    query = db.query(Contact)
+async def get_contacts(user: User, db: Session, skip: int, limit: int, favorite: Union[bool, None] = None):
+    query = db.query(Contact).filter_by(user_id=user.id)
     if favorite is not None:
         query = query.filter_by(favorite=favorite)
     contact = query.offset(skip).limit(limit).all()
     return contact
 
 
-async def get_contact_by_id(contact_id: int, db: Session):
-    contact = db.query(Contact).filter_by(id=contact_id).first()
+async def get_contact_by_id(contact_id: int, db: Session, user: User):
+    contact = db.query(Contact).filter(and_(Contact.id == contact_id, Contact.user_id == user.id)).first()
     return contact
 
 
-async def get_contact_by_email(email: str, db: Session):
-    contact = db.query(Contact).filter_by(email=email).first()
+async def get_contact_by_email(email: str, db: Session, user: User):
+    contact = db.query(Contact).filter(and_(Contact.email == email, Contact.user_id == user.id)).first()
     return contact
 
 
-async def create(body: ContactModel, db: Session):
+async def create(body: ContactModel, db: Session, user: User):
     contact = Contact(
         first_name=body.first_name,
         last_name=body.last_name,
@@ -35,6 +35,7 @@ async def create(body: ContactModel, db: Session):
         birthday=body.birthday,
         comments=body.comments,
         favorite=body.favorite,
+        user_id=user.id
     )
     db.add(contact)
     db.commit()
@@ -42,8 +43,8 @@ async def create(body: ContactModel, db: Session):
     return contact
 
 
-async def update(contact_id: int, body: ContactModel, db: Session):
-    contact = await get_contact_by_id(contact_id, db)
+async def update(contact_id: int, body: ContactModel, db: Session, user: User):
+    contact = await get_contact_by_id(contact_id, db, user)
     if contact:
         contact.first_name = body.first_name
         contact.last_name = body.last_name
@@ -56,24 +57,24 @@ async def update(contact_id: int, body: ContactModel, db: Session):
     return contact
 
 
-async def favorite_update(contact_id: int, body: ContactModel, db: Session):
-    contact = await get_contact_by_id(contact_id, db)
+async def favorite_update(contact_id: int, body: ContactModel, db: Session, user: User):
+    contact = await get_contact_by_id(contact_id, db, user)
     if contact:
         contact.favorite = body.favorite
         db.commit()
     return contact
 
 
-async def delete(contact_id, db: Session):
-    contact = await get_contact_by_id(contact_id, db)
+async def delete(contact_id, db: Session, user: User):
+    contact = await get_contact_by_id(contact_id, db, user)
     if contact:
         db.delete(contact)
         db.commit()
     return contact
 
 
-async def search_contacts(query: str, db: Session):
-    contacts = db.query(Contact).filter(
+async def search_contacts(query: str, db: Session, user: User):
+    contacts = db.query(Contact).filter_by(user_id=user.id).filter(
         func.lower(Contact.first_name).contains(func.lower(query)) |
         func.lower(Contact.last_name).contains(func.lower(query)) |
         func.lower(Contact.email).contains(func.lower(query))
@@ -81,13 +82,13 @@ async def search_contacts(query: str, db: Session):
     return contacts
 
 
-async def search_birthday(par: dict, db: Session):
+async def search_birthday(par: dict, db: Session, user: User):
     days_param = par.get("days", 7)
     days = int(days_param)
     days += 1
     now = datetime.now().date()
     birthdays_contacts = []
-    query = db.query(Contact)
+    query = db.query(Contact).filter_by(user_id=user.id)
     contacts = query.offset(par.get("skip")).limit(par.get("limit"))
 
     for contact in contacts:
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -5,9 +5,10 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
-from src.schemas import ContactModel, ContactFavoriteModel, ContactResponse
+from src.database.models import User
 from src.repository import contacts as repos_contacts
-
+from src.services.auth import auth_service
+from src.schemas import ContactModel, ContactFavoriteModel, ContactResponse
 
 router = APIRouter(prefix="/contacts", tags=["contact"])
 
@@ -17,28 +18,32 @@ async def get_contact(
         skip: int = 0,
         limit: int = Query(default=10, le=100, ge=10),
         favorite: bool = None,
-        db: Session = Depends(get_db), ):
-    contacts = await repos_contacts.get_contacts(db=db, skip=skip, limit=limit, favorite=favorite)
+        db: Session = Depends(get_db),
+        current_user: User = Depends(auth_service.get_current_user)):
+    contacts = await repos_contacts.get_contacts(user=current_user, db=db, skip=skip,
+                                                 limit=limit, favorite=favorite)
     return contacts
 
 
 @router.get("/{contact_id}", response_model=ContactResponse)
-async def get_contact(contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
-    contact = await repos_contacts.get_contact_by_id(contact_id, db)
+async def get_contact(contact_id: int = Path(ge=1), db: Session = Depends(get_db),
+                      current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repos_contacts.get_contact_by_id(contact_id, db, current_user)
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
     return contact
 
 
 @router.post("", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
-async def create_contact(body: ContactModel, db: Session = Depends(get_db)):
-    contact = await repos_contacts.get_contact_by_email(body.email, db)
+async def create_contact(body: ContactModel, db: Session = Depends(get_db),
+                         current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repos_contacts.get_contact_by_email(body.email, db, current_user)
     if contact:
         raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT, detail=f"Email is exist!"
+            status_code=status.HTTP_409_CONFLICT, detail=f"Email already exists!"
         )
     try:
-        contact = await repos_contacts.create(body, db)
+        contact = await repos_contacts.create(body, db, current_user)
     except IntegrityError as err:
         raise HTTPException(
             status_code=status.HTTP_404_INVALID_REQUEST, detail=f"Error: {err}"
@@ -47,29 +52,31 @@ async def create_contact(body: ContactModel, db: Session = Depends(get_db)):
 
 
 @router.put("/{contact_id}", response_model=ContactResponse)
-async def update_contact(
-        body: ContactModel, contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
-    contact = await repos_contacts.update(contact_id, body, db)
+async def update_contact(body: ContactModel,
+                         contact_id: int = Path(ge=1),
+                         db: Session = Depends(get_db),
+                         current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repos_contacts.update(contact_id, body, db, current_user)
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
     return contact
 
 
 @router.patch("/{contact_id}/favorite", response_model=ContactResponse)
-async def favorite_update(
-        body: ContactFavoriteModel,
-        contact_id: int = Path(ge=1),
-        db: Session = Depends(get_db)
-):
-    contact = await repos_contacts.favorite_update(contact_id, body, db)
+async def favorite_update(body: ContactFavoriteModel,
+                          contact_id: int = Path(ge=1),
+                          db: Session = Depends(get_db),
+                          current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repos_contacts.favorite_update(contact_id, body, db, current_user)
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
     return contact
 
 
 @router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def remove_contact(contact_id: int = Path(ge=1), db: Session = Depends(get_db)):
-    contact = await repos_contacts.delete(contact_id, db)
+async def remove_contact(contact_id: int = Path(ge=1), db: Session = Depends(get_db),
+                         current_user: User = Depends(auth_service.get_current_user)):
+    contact = await repos_contacts.delete(contact_id, db, current_user)
     if contact is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
     return None
@@ -77,9 +84,9 @@ async def remove_contact(contact_id: int = Path(ge=1), db: Session = Depends(get
 
 @router.get("/search/{query}", response_model=List[ContactResponse],
             summary="Search contacts by name, lastname, email")
-async def search_by(query: Optional[str] = None,
-                    db: Session = Depends(get_db)):
-    contacts = await repos_contacts.search_contacts(query, db)
+async def search_by(query: Optional[str] = None, db: Session = Depends(get_db),
+                    current_user: User = Depends(auth_service.get_current_user)):
+    contacts = await repos_contacts.search_contacts(query, db, current_user)
     if not contacts:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Contact Not Found")
 
@@ -87,12 +94,11 @@ async def search_by(query: Optional[str] = None,
 
 
 @router.get("/birthdays/next_week", response_model=List[ContactResponse])
-async def search_contacts(
-        days: int = Query(default=7, le=30, ge=1),
-        skip: int = 0,
-        limit: int = Query(default=10, le=30, ge=1),
-        db: Session = Depends(get_db)
-):
+async def search_contacts(days: int = Query(default=7, le=30, ge=1),
+                          skip: int = 0,
+                          limit: int = Query(default=10, le=30, ge=1),
+                          db: Session = Depends(get_db),
+                          current_user: User = Depends(auth_service.get_current_user)):
     contacts = None
     if days:
         par = {
@@ -100,7 +106,7 @@ async def search_contacts(
             "skip": skip,
             "limit": limit,
         }
-        contacts = await repos_contacts.search_birthday(par, db)
+        contacts = await repos_contacts.search_birthday(par, db, current_user)
     if not contacts:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No contacts found")
     return contacts
