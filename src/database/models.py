c39459250ca9256ecc099b3a469a237443869a81
Vasyl Boliukh <89039235+Vskesha@users.noreply.github.com>
2024-01-06 17:05:27 +0200

    added User to database models and made migration

--- src/database/models.py
+++ src/database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String, DateTime, Date, Text, func
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Boolean, Column, Date, DateTime, ForeignKey, func, Integer, String, Text
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -17,3 +17,16 @@ class Contact(Base):
     favorite = Column(Boolean, default=False)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
+    user_id = Column('user_id', ForeignKey('users.id', ondelete='CASCADE'), default=None)
+    user = relationship('User', backref="contacts")
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50))
+    email = Column(String(250), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    created_at = Column('crated_at', DateTime, default=func.now())
+    avatar = Column(String(255), nullable=True)
+    refresh_token = Column(String(255), nullable=True)
